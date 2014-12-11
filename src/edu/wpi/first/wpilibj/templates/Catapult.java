536a3985d77f0698c20c1da9aa715eb540af0926
Rarstyak <9724@stu.zps.org>
2014-12-11 18:24:45 -0500

    Changed speeds and encoder counts for catapult

--- src/edu/wpi/first/wpilibj/templates/Catapult.java
+++ src/edu/wpi/first/wpilibj/templates/Catapult.java
@@ -27,7 +27,7 @@ public class Catapult {
     
     private final double CAM_RELEASE_SPEED = 0.45;
     private final double CAM_FAST_SPEED = 1.0;
-    private final double CAM_SLOW_SPEED = 1.0;
+    private final double CAM_SLOW_SPEED = .8;
     private boolean firing = false;
     
     
@@ -69,7 +69,7 @@ public class Catapult {
                     setCamMotors(CAM_RELEASE_SPEED);
                     firing = true;
                 } else if (firing) {
-                    if (camEncoderCount >= 50 && camEncoderCount <= 205) {
+                    if (camEncoderCount >= 50 && camEncoderCount <= 195) {
                         setCamMotors(CAM_SLOW_SPEED);
                     } else if (camEncoderCount > 250 || camEncoderCount < 50) {
                         setCamMotors(CAM_FAST_SPEED);
